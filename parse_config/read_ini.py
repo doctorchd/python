8f35e2c2a1abad957173bdd0d2d710133c1cae2a
Dmytro Cherkasov <doctorchd@gmail.com>
2022-09-19 15:44:21 +0300

    read ini

--- parse_config/read_ini.py
+++ parse_config/read_ini.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+https://docs.python.org/3/library/configparser.html
+"""
+
 import configparser
 
 # -----------------------------------------------------------------------------
@@ -15,7 +19,13 @@ TEST_FILE = "test.conf"
 # action
 # -----------------------------------------------------------------------------
 config = configparser.ConfigParser()
-config.read(TEST_FILE)
+# config.read(TEST_FILE)
+try:
+    with open(TEST_FILE, 'r') as f:
+        config.read_file(f)
+except Exception as exc:
+    print(exc)
+
 print(type(config))
 print(type(config.sections()))
 for section in config.sections():
