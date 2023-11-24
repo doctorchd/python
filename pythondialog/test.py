8f1207724facad9cc6174b866617b344ad8b3a39
Dmytro Cherkasov <doctorchd@gmail.com>
2023-11-25 00:08:30 +0200

    pythondialog (continuing)

--- pythondialog/test.py
+++ pythondialog/test.py
@@ -5,7 +5,7 @@ import time
 from dialog import Dialog
 
 # This is almost always a good thing to do at the beginning of your programs.
-locale.setlocale(locale.LC_ALL, '')
+locale.setlocale(locale.LC_ALL, 'uk_UA.UTF-8')
 
 # You may want to use 'autowidgetsize=True' here (requires pythondialog >= 3.1)
 d = Dialog(dialog="dialog", autowidgetsize=True)
@@ -40,12 +40,21 @@ while True:
             ("(11)", "pause()"),
             ("(12)", "regular gauge"),
             ("(13)", "mixed gauge"),
+            ("(14)", "buildlist()"),
+            ("(15)", "radiolist()"),
+            ("(16)", "treeview()"),
+            ("(17)", "inputbox()"),
+            ("(18)", "inputmenu()"),
+            ("(19)", "passwordbox()"),
+            ("(20)", "form()"),
+            ("(21)", "mixedform()"),
+            ("(22)", "passwordform()"),
             ("(0)", "exit")
         ],
         help_button=True
     )
 
-    input(f'You pressed {button_names[code]} button')
+    # input(f'You pressed {button_names[code]} button')
 
     if code == d.OK:
         if tag == "(0)":
@@ -66,19 +75,19 @@ while True:
                 item_help=True,
                 help_tags=True
             )
-            input(f'You pressed {button_names[code]} button, tag: {tag}')
+            # input(f'You pressed {button_names[code]} button, tag: {tag}')
             # if code == d.OK:
             #     input(f'{tag} was chosen')
 
         elif tag == "(2)":
             # check msgbox()
             code = d.msgbox('This is a message box', title='Test message box')
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(3)":
             # check infobox()
             code = d.infobox('This is an infobox', height =20, width=20, title='Test infobox')
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(4)":
             # check checklist()
@@ -95,12 +104,12 @@ while True:
                 help_tags=True,
                 help_status=True
             )
-            input(f'You pressed {button_names[code]} button, tag: {tag}')
+            # input(f'You pressed {button_names[code]} button, tag: {tag}')
 
         elif tag == "(5)":
             # check textbox()
             code = d.textbox('test.txt')
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(6)":
             # check scrollbox()
@@ -124,27 +133,27 @@ while True:
                 'test test test test test test test test test test '
                 """
             )
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(7)":
             # check editbox()
             code, text = d.editbox('test.txt')
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(8)":
             # check progressbox()
             code = d.progressbox('/var/log/syslog')
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(9)":
             # check programbox()
             code = d.programbox('/var/log/syslog')
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(10)":
             # check tailbox()
             code = d.tailbox('/var/log/syslog')
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(11)":
             # check pause()
@@ -152,7 +161,7 @@ while True:
                 'Pausing 20 sec',
                 seconds=20
             )
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(12)":
             # check regular gauge
@@ -179,7 +188,7 @@ while True:
             d.gauge_update(100, text='completed', update_text=True)
             d.gauge_stop()
 
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
 
         elif tag == "(13)":
             # check mixed gauge
@@ -191,7 +200,7 @@ while True:
                     ('test3', '-0'),
                     ('test4', '-0')
                 ],
-                percent='0',
+                percent=0,
                 text='example'
             )
             time.sleep(2)
@@ -203,7 +212,7 @@ while True:
                     ('test3', '-0'),
                     ('test4', '-100')
                 ],
-                percent='20',
+                percent=20,
                 text='example'
             )
             time.sleep(2)
@@ -215,7 +224,7 @@ while True:
                     ('test3', '-0'),
                     ('test4', '-100')
                 ],
-                percent='40',
+                percent=40,
                 text='example'
             )
             time.sleep(2)
@@ -227,14 +236,169 @@ while True:
                     ('test3', '0'),
                     ('test4', '-90')
                 ],
-                percent='80',
+                percent=80,
                 text='example'
             )
 
-            input(f'You pressed {button_names[code]} button')
+            # input(f'You pressed {button_names[code]} button')
+
+        elif tag == "(14)":
+            # check buildlist()
+            code, tag = d.buildlist(
+                "This is a buildlist",
+                items=[
+                    ("choice 1", "tag 1", False, "help1"),
+                    ("choice 2", "tag 2", True, "help2"),
+                    ("choice 3", "tag 3", True, "help3"),
+                    ("choice 4", "tag 4", False, "help4")
+                ],
+                help_button=True,
+                item_help=True,
+                help_tags=True,
+                help_status=True
+            )
+            # input(f'You pressed {button_names[code]} button, tag: {tag}')
+
+        elif tag == "(15)":
+            # check radiolist()
+            code, tag = d.radiolist(
+                "Choose some item",
+                title="Test Radio List",
+                choices=[
+                    ("1", "choice 1", True, "Help1"),
+                    ("2", "choice 2", False, "Help2"),
+                    ("3", "choice 3", False, "Help3"),
+                    ("4", "choice 4", False, "Help4"),
+                ],
+                help_button=True,
+                item_help=True,
+                help_tags=True
+            )
+            # input(f'You pressed {button_names[code]} button, tag: {tag}')
+            # if code == d.OK:
+            #     input(f'{tag} was chosen')
+
+        elif tag == "(16)":
+            # check treeview()
+            code, tag = d.treeview(
+                "Choose some item",
+                title="Test Radio List",
+                nodes=[
+                    ("1", "choice 1", True, 1, "Help1"),
+                    ("2", "choice 2", False, 2, "Help2"),
+                    ("3", "choice 3", False, 3, "Help3"),
+                    ("4", "choice 4", False, 2, "Help4"),
+                ],
+                help_button=True,
+                item_help=True,
+                help_tags=True
+            )
+            # input(f'You pressed {button_names[code]} button, tag: {tag}')
+            # if code == d.OK:
+            #     input(f'{tag} was chosen')
+
+        elif tag == "(17)":
+            # check inputbox()
+            code, string = d.inputbox(
+                "Enter something",
+                title="Test Input Box",
+                init='',
+                help_button=True,
+                item_help=True,
+                help_tags=True
+            )
+            # input(f'You pressed {button_names[code]} button, string: {string}')
+
+        elif tag == "(18)":
+            # check inputmenu()
+            exit_info, tag, new_item_text = d.inputmenu(
+                "Enter something",
+                title="Test Input Menu",
+                choices=[
+                    ("1", "choice 1", "Help1"),
+                    ("2", "choice 2", "Help2"),
+                    ("3", "choice 3", "Help3"),
+                    ("4", "choice 4", "Help4")
+                ],
+                help_button=True,
+                item_help=True,
+                help_tags=True
+            )
+            # input(f'You got exit_info: {exit_info}, tag: {tag}, new_item_text: {new_item_text}')
+
+        elif tag == "(19)":
+            # check inputbox()
+            code, password = d.passwordbox(
+                "Enter password",
+                title="Test Password Box",
+                init='qwerty123',
+                insecure=True,
+                help_button=True,
+                item_help=True,
+                help_tags=True
+            )
+            # input(f'You pressed {button_names[code]} button, password: {password}')
+
+        elif tag == "(20)":
+            # check form()
+            code, list = d.form(
+                "Fill the Form",
+                # height=20,
+                # width=40,
+                # form_height=8,
+                title="Test Form",
+                # help_button=True,
+                # item_help=True,
+                # help_tags=True,
+                elements=[
+                    ("label1", 1, 1, "item1", 1, 10, 10, 20),
+                    ("label2", 2, 1, "item2", 2, 10, 10, 20),
+                    ("label3", 3, 1, "item3", 3, 10, 10, 20),
+                    ("label4", 4, 1, "item4", 4, 10, 10, 20),
+                    ('label5', 5, 1, "item5", 5, 10, 10, 20)
+                ]
+            )
+            # input(f'You pressed {button_names[code]} got list: {list}')
+
+        elif tag == "(21)":
+            # check mixedform()
+            code, list = d.mixedform(
+                "Fill the Form",
+                title="Test Mixed Form",
+                elements=[
+                    ("label1", 1, 1, "item1", 1, 10, 10, 20, 0, ''),
+                    ("label2", 2, 1, "item2", 2, 10, 10, 20, 0, ''),
+                    ("label3", 3, 1, "item3", 3, 10, 10, 20, 0, ''),
+                    ("label4", 4, 1, "item4", 4, 10, 10, 20, 1, '')
+                ],
+                help_button=True,
+                item_help=True,
+                help_tags=True,
+                insecure=True
+            )
+            # input(f'You pressed {button_names[code]} got list: {list}')
+
+        elif tag == "(22)":
+            # check passwordform()
+            code, list = d.passwordform(
+                "Fill the Form",
+                title="Test Password Form",
+                elements=[
+                    ("label1", 1, 1, "item1", 1, 10, 10, 20, 'aaa'),
+                    ("label2", 2, 1, "item2", 2, 10, 10, 20, 'bbb'),
+                    ("label3", 3, 1, "item3", 3, 10, 10, 20, 'ccc'),
+                    ("label4", 4, 1, "item4", 4, 10, 10, 20, 'ddd')
+                ],
+                help_button=True,
+                item_help=True,
+                help_tags=True,
+                insecure=True
+            )
+            # input(f'You pressed {button_names[code]} got list: {list}')
 
     elif code == d.ESC:
-        print('ESC was pressed')
+        # print('ESC was pressed')
+        pass
 
     else:
         break
